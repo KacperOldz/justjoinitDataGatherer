9d4623e2516cd890890dfeedb49080bd56b9a2aa
Kacper <87307561+KacperOldz@users.noreply.github.com>
2024-04-20 12:01:28 +0200

    Update code.py

--- code.py
+++ code.py
@@ -2,13 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 import re
+import json
 
-# Constants
 BASE_URL = "https://justjoin.it"
-LOCATIONS = ["/all-locations", "/bialystok"]
-TECHS = ["/java", "/javascript", "/python"]
 EXPERIENCE = ["/", "/experience-level_junior?index=0#more-filters"]
 
+def readInfo():
+    global LOCATIONS, TECHS
+    with open('config.json', 'r') as file:
+        json_data = file.read()
+        data = json.loads(json_data)
+    LOCATIONS = data["locations"]
+    print(LOCATIONS)
+    TECHS = [lang["link"] for lang in data["languages"]]
 def extract_numbers_from_string(input_string):
     """Extracts numbers from a string."""
     numbers = re.findall(r'\d+', input_string)
@@ -26,12 +32,13 @@ def fetch_page_content(url):
 def find_element_content(html):
     """Finds and extracts content from a specific element."""
     soup = BeautifulSoup(html, 'html.parser')
-    element = soup.find(class_="css-ggjav7").find(class_="css-9hflmo")
-    if element:
-        return extract_numbers_from_string(element.text)
-    else:
-        print("Element not found.")
-        return None
+    parent_element = soup.find(class_="css-ggjav7")
+    if parent_element:
+        element = parent_element.find(class_="css-9hflmo")
+        if element:
+            return extract_numbers_from_string(element.text)
+    print("Element not found.")
+    return 0
 
 def save_data_to_file(content, filename):
     """Saves data to a file."""
@@ -40,6 +47,7 @@ def save_data_to_file(content, filename):
 
 def main():
     """Main function to scrape data and save it."""
+    readInfo()
     for location in LOCATIONS:
         description = "\n" + datetime.datetime.now().strftime("%Y-%m-%d")
         for experience_level in EXPERIENCE:
