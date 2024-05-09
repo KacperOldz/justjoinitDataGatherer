4f2ecef53f7eb41fc0f45f8b98185cdedd140af6
Kacper <87307561+kacperdev0@users.noreply.github.com>
2024-05-09 20:22:21 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,58 +1,57 @@
 import tkinter as tk
 from tkinter import filedialog
-import code
+import main
 
-# Default file paths and directories
 actualDir = "./saves/save1"
 fileName = "all-locations.txt"
 saveDir = "saves/save1"
-configDir = "config.json"
+configDirLabel = "config.json"
 
 def select_txt_file():
-    """Function to open a file dialog and select a .txt file."""
     global fileName
     fileName = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    txtDirLabel.config(text=fileName)
 
 def select_config_file():
-    """Function to open a file dialog and select a .json configuration file."""
     global configDir
     configDir = filedialog.askopenfilename(filetypes=[("Json files", "*.json")])
+    configDirLabel.config(text=configDir)
 
 def select_folder_file():
-    """Function to open a file dialog and select a directory for saving files."""
     global saveDir
     saveDir = filedialog.askdirectory()
+    folderDirLabel.config(text=saveDir)
+
 
 def drawGraphButton():
-    """Function to draw a graph based on selected data and configuration."""
     data = main.readData(fileName)
-    colors, languages = main.getGraphColorsAndLabels(configDir)
+    colors, languages = main.getGraphColorsAndLabels(configDirLabel)
     main.drawGraph(colors, languages, data)
 
 def getDataButton():
-    """Function to get data from a website based on configuration and save it."""
     LOCATIONS, TECHS, EXPERIENCE = main.getDesiredLanguagesTechsAndExperience(configDir)
     print(LOCATIONS, TECHS, EXPERIENCE)
     main.getAndSaveDataFromWebside(LOCATIONS, TECHS, EXPERIENCE, saveDir)
 
 def createGUI():
-    """Function to create the graphical user interface."""
-    # Buttons for selecting files and directories
+    global txtDirLabel, configDirLabel, folderDirLabel
     select_button = tk.Button(root, text="Select .txt File", command=select_txt_file)
-    select_button.pack(padx=10, pady=10)
+    select_button.grid(row=0, column=0,padx=10,pady=10)
+    txtDirLabel = tk.Label(root, text="directiory")
+    txtDirLabel.grid(row=0, column=1, padx=10, pady=10)
 
     select_config_button = tk.Button(root, text="Select .json File", command=select_config_file)
-    select_config_button.pack(padx=10, pady=10)
+    select_config_button.grid(row=1,column=0,padx=10,pady=10)
+    configDirLabel = tk.Label(root, text="directiory")
+    configDirLabel.grid(row=1, column=1, padx=10, pady=10)
 
     select_folder_button = tk.Button(root, text="Select Save Folder", command=select_folder_file)
-    select_folder_button.pack(padx=10, pady=10)
+    select_folder_button.grid(row=2,column=0,padx=10,pady=10)
+    folderDirLabel = tk.Label(root, text="directiory")
+    folderDirLabel.grid(row=2, column=1, padx=10, pady=10)
 
-    # Buttons for executing actions
     execute_button = tk.Button(root, text="Draw Graph", command=drawGraphButton)
-    execute_button.pack(padx=10, pady=10)
-
     execute_button = tk.Button(root, text="Get Data", command=getDataButton)
-    execute_button.pack(padx=10, pady=10)
 
 # Create the main window
 root = tk.Tk()
